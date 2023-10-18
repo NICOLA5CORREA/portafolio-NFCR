77c9e87d074688d2e781e7cd1707626608fdab5b
NICOLAS CORREA <134348375+NICOLA5CORREA@users.noreply.github.com>
2023-10-17 21:29:06 -0500

    scrolled

--- src/js/textype.js
+++ src/js/textype.js
@@ -50,8 +50,8 @@
             }
         }
         // INJECT CSS
-        // var css = document.createElement("style");
-        // css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-        // document.body.appendChild(css);
+        var css = document.createElement("style");
+        css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+        document.body.appendChild(css);
     };
 })();
