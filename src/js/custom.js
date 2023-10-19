99587c9271ea0966cf24a04bfb9cdec270a0ec83
NICOLAS CORREA <134348375+NICOLA5CORREA@users.noreply.github.com>
2023-10-18 20:38:50 -0500

    scroll-txtype

--- src/js/custom.js
+++ src/js/custom.js
@@ -1,6 +1,6 @@
-(function($) {
 
-  // Init Wow
+(function($) {
+  // Inicializar Wow
   wow = new WOW({
     animateClass: 'animated',
     offset: 100
@@ -12,7 +12,7 @@
   });
 
   // Navigation scrolls
-  $('.navbar-nav li a').bind('click', function(event) {
+  $('.navbar-nav li a').on('click', function(event) {
     $('.navbar-nav li').removeClass('active');
     $(this).closest('li').addClass('active');
     var $anchor = $(this);
@@ -21,20 +21,16 @@
       $('html, body').stop().animate({
         scrollTop: $($anchor.attr('href')).offset().top
       }, 1500, 'easeInOutExpo');
-
       event.preventDefault();
     }
   });
 
-
-  //jQuery to collapse the navbar on scroll
-  $(window).scroll(function() {
+  // jQuery para colapsar la barra de navegación al desplazarse
+  $(window).on('scroll', function() {
     if ($(".navbar-default").offset().top > 50) {
       $(".navbar-fixed-top").addClass("top-nav-collapse");
     } else {
       $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
-
   });
-
 })(jQuery);
--- src/js/scroll.js
+++ src/js/scroll.js
@@ -2,7 +2,7 @@
     // Función para controlar el cambio de color al hacer scroll
     window.addEventListener('scroll', function() {
         var nav = document.querySelector('.nav');
-        if (window.scrollY > 150) { // Cambia 100 a la posición deseada donde quieres que ocurra el cambio
+        if (window.scrollY > 120) { // Cambia 100 a la posición deseada donde quieres que ocurra el cambio
             nav.classList.add('scrolled');
         } else {
             nav.classList.remove('scrolled');
